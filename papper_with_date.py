362f1492db2bf69f6631e9dc4416e973d21482fc
Arthur Sendas F Almeida <73764748+guerraepaz@users.noreply.github.com>
2023-09-04 20:12:30 -0300

    Update papper_with_date.py

--- papper_with_date.py
+++ papper_with_date.py
@@ -1,53 +1,53 @@
 Artigos:
-A Smartphone-Based Mobility Assistant Using Depth Imaging for Visually Impaired and Blind
-Android Based Object Recognition for visually impaired
-Efficient Multi-Object Detection and Smart Navigation Using Artificial Intelligence for Visually Impaired People
-Real Time Object Detection with Audio Feedback using Yolo vs. Yolo_v3
-Real-Time Object Detection And Identification For Visually Challenged People Using Mobile Platform
-Object Detection and Narrator for Visually Impaired People
-Real-Time Sign Detection for Accessible Indoor Navigation
-Object Detection Using Machine Learning for Visually Impaired People
-Indoor Signs Detection for Visually Impaired People: Navigation Assistance Based on a Lightweight Anchor-Free Object Detector
-IoT Enabled Automated Object Recognition for the Visually Impaired
-Virtual AI Assistant for Person with Partial Vision Impairment 
-REAL TIME OBJECT DETECTION AND RECOGNITION SYSTEM TO ASSIST THE VISUALLY IMPAIRED
-Guiding Aid for Visually Impaired
-Assistive Object Recognition System for Visually Impaired
-Electronic Orientation Aid for Visually Impaired using Graphics Processing Unit (GPU)
-Deep Learning Reader for Visually Impaired
-Real-time Object Detection and Recognition Using Deep Learning with YOLO Algorithm for Visually Impaired People
-Real Time Object Detection Using YOLO v3 Tiny with Voice Feedback for Visually Impaired
-Development of a Safe Walking Assistance System for Visually Impaired Persons Using MY VISION
-Design and Implementation of an Embedded Real-Time System for Guiding Visually Impaired Individuals
-A Blind navigation method in indoors/outdoors areas
-Implementation of a Blind navigation method in outdoors/indoors areas
-Assisting Blind People Using Object Detection with Vocal Feedback
-Deblur-YOLO: Real-Time Object Detection with Efficient Blind Motion Deblurring
-Web-based object detection and sound feedback system for visually impaired people
-A Frame-work assisting the Visually Impaired People: Common Object Detection and Pose Estimation in Surrounding Environment
-Seeing Without Sight — An Automatic Cognition System Dedicated to Blind and Visually Impaired People
-ORCap: Object Recognition Cap (A navigation system for the blind)
-Design and Implementation of Real-time Object Detection for Blind using Convolutional Neural Network
-A Mobile Application For The Visually Handicapped
-Guiding Visually Impaired People to Find an Object by Using Image to Speech over the Smart Phone Cameras
-SceneRecog: A Deep Learning Scene Recognition Model for Assisting Blind and Visually Impaired Navigate using Smartphones
-Computer vision assisted, real time blind spot detection based collision warning system for two wheelers
-Smartphone Based Tactile Feedback System Providing Navigation and Obstacle Avoidance to the Blind and Visually Impaired
-My Mobile Knows That I am Driving! In-Vehicle (Relative) Blind Localization of a Smartphone
-Intelligent Assistance System for Visually Impaired/Blind People (ISVB)
-Object Detection and Video Analyser for the Visually Impaired
-Smart Vision based Assistant for Visually Impaired
-Reforming Warning and Obstacle Detection Assisting Visually Impaired People on mHealth
-Real-Time Object Detection for Visually Challenged People
-Single Shot Detector Based Object Detection For Visually Impaired People
-Blind Lane Detection Algorithm Based on Improved YOLOv5s
-Obstacle Avoidance for blind people using Yolo algorithm, Darknet and GTTS
-Blind Assistance System using Tensor Flow
-Smart Navigation Detection using Deep-learning for Visually Impaired Person
-Guardian - Smart Assistant Tool for Visually Impaired People
-Road Pothole Detection Algorithm and Guide Belt Designed for Visually Impaired
-An Real Time Object Detection Method for Visually Impaired Using Machine Learning
-
+A Smartphone-Based Mobility Assistant Using Depth Imaging for Visually Impaired and Blind - 2022 - with dataset COCO
+Real Time Object Detection with Audio Feedback using Yolo vs. Yolo_v3 - 2021 - with dataset COCO
+Real-Time Object Detection And Identification For Visually Challenged People Using Mobile Platform - 2021 - with Dataset Coco end Dota
+Virtual AI Assistant for Person with Partial Vision Impairment - 2021 - with dataset COCO
+REAL TIME OBJECT DETECTION AND RECOGNITION SYSTEM TO ASSIST THE VISUALLY IMPAIRED - 2021 - with dataset COCO
+Guiding Aid for Visually Impaired - 2022 - with dataset COCO
+Assistive Object Recognition System for Visually Impaired - 2020 - with dataset COCO
+Electronic Orientation Aid for Visually Impaired using Graphics Processing Unit (GPU) - 2020 - with dataset COCO
+Deblur-YOLO: Real-Time Object Detection with Efficient Blind Motion Deblurring - 2021 - with dataset COCO
+ORCap: Object Recognition Cap (A navigation system for the blind) - 2020 - with dataset COCO
+Design and Implementation of Real-time Object Detection for Blind using Convolutional Neural Network - 2022 - with dataset COCO
+A Mobile Application For The Visually Handicapped - 2021 - with dataset COCO
+Guiding Visually Impaired People to Find an Object by Using Image to Speech over the Smart Phone Cameras - 2021 - with dataset COCO
+Intelligent Assistance System for Visually Impaired/Blind People (ISVB) - 2022 - with dataset COCO
+Object Detection and Video Analyser for the Visually Impaired - 2023 - with dataset COCO
+Smart Vision based Assistant for Visually Impaired - 2022 -  with dataset COCO
+Reforming Warning and Obstacle Detection Assisting Visually Impaired People on mHealth - 2019 - with dataset COCO
+Real-Time Object Detection for Visually Challenged People - 2020 - with dataset COCO
+Single Shot Detector Based Object Detection For Visually Impaired People - 2022 - with dataset COCO
+Obstacle Avoidance for blind people using Yolo algorithm, Darknet and GTTS - 2023 - with dataset COCO
+An Real Time Object Detection Method for Visually Impaired Using Machine Learning - 2023 - with dataset COCO
+Android Based Object Recognition for visually impaired - 2020 - without dataset
+Efficient Multi-Object Detection and Smart Navigation Using Artificial Intelligence for Visually Impaired People - 2020 ???
+Object Detection and Narrator for Visually Impaired People - 2019 - with Dataset Imagenet
+Real-Time Sign Detection for Accessible Indoor Navigation - 2021 - without dataset
+Object Detection Using Machine Learning for Visually Impaired People - 2020 - without dataset
+Indoor Signs Detection for Visually Impaired People: Navigation Assistance Based on a Lightweight Anchor-Free Object Detector - 2023 - wiyh dataset Pascal VOC
+IoT Enabled Automated Object Recognition for the Visually Impaired - 2021 - without dataset 
+Deep Learning Reader for Visually Impaired - 2022 - with dataset Flickr 8K
+Real-time Object Detection and Recognition Using Deep Learning with YOLO Algorithm for Visually Impaired People  ???
+Real Time Object Detection Using YOLO v3 Tiny with Voice Feedback for Visually Impaired  ???
+Development of a Safe Walking Assistance System for Visually Impaired Persons Using MY VISION - 2023 - without dataset
+Design and Implementation of an Embedded Real-Time System for Guiding Visually Impaired Individuals - 2019 - without dataset
+A Blind navigation method in indoors/outdoors areas - 2022 - with dataset ImageNet
+Implementation of a Blind navigation method in outdoors/indoors areas - 2022 - with dataset ImageNet
+Assisting Blind People Using Object Detection with Vocal Feedback - 2022 - with dataset Open images datase
+Web-based object detection and sound feedback system for visually impaired people - 2020 - with dataset ImageNet
+A Frame-work assisting the Visually Impaired People: Common Object Detection and Pose Estimation in Surrounding Environment - 2018 - without dataset
+Seeing Without Sight — An Automatic Cognition System Dedicated to Blind and Visually Impaired People - 2017 - without dataset 
+SceneRecog: A Deep Learning Scene Recognition Model for Assisting Blind and Visually Impaired Navigate using Smartphones - 2021 - without dataset
+Computer vision assisted, real time blind spot detection based collision warning system for two wheelers - 2021 - without dataset
+Smartphone Based Tactile Feedback System Providing Navigation and Obstacle Avoidance to the Blind and Visually Impaired - 2022 - without dataset
+My Mobile Knows That I am Driving! In-Vehicle (Relative) Blind Localization of a Smartphone - 2022 - without dataset
+Blind Lane Detection Algorithm Based on Improved YOLOv5s - 2023 - without dataset
+Blind Assistance System using Tensor Flow - 2022 - without dataset
+Smart Navigation Detection using Deep-learning for Visually Impaired Person - 2021 - without dataset
+Guardian - Smart Assistant Tool for Visually Impaired People - 2022 - without dataset
+Road Pothole Detection Algorithm and Guide Belt Designed for Visually Impaired - 2023 - with dataset Kaggle
+Android Smartphone Based Visual Object Recognition for Visually Impaired Using Deep Learning - 2018 - without dataset
 
 
 
