4ddccb27401b8213319bce1b731c62e866dddc8b
Arthur Sendas F Almeida <73764748+guerraepaz@users.noreply.github.com>
2023-09-04 18:34:11 -0300

    Create artigos.py

---
+++ artigos.py
@@ -0,0 +1,97 @@
+Artigos:
+A Smartphone-Based Mobility Assistant Using Depth Imaging for Visually Impaired and Blind
+Android Based Object Recognition for visually impaired
+Efficient Multi-Object Detection and Smart Navigation Using Artificial Intelligence for Visually Impaired People
+Real Time Object Detection with Audio Feedback using Yolo vs. Yolo_v3
+Real-Time Object Detection And Identification For Visually Challenged People Using Mobile Platform
+Object Detection and Narrator for Visually Impaired People
+Real-Time Sign Detection for Accessible Indoor Navigation
+Object Detection Using Machine Learning for Visually Impaired People
+Indoor Signs Detection for Visually Impaired People: Navigation Assistance Based on a Lightweight Anchor-Free Object Detector
+IoT Enabled Automated Object Recognition for the Visually Impaired
+Virtual AI Assistant for Person with Partial Vision Impairment 
+REAL TIME OBJECT DETECTION AND RECOGNITION SYSTEM TO ASSIST THE VISUALLY IMPAIRED
+Guiding Aid for Visually Impaired
+Assistive Object Recognition System for Visually Impaired
+Electronic Orientation Aid for Visually Impaired using Graphics Processing Unit (GPU)
+Deep Learning Reader for Visually Impaired
+Real-time Object Detection and Recognition Using Deep Learning with YOLO Algorithm for Visually Impaired People
+Real Time Object Detection Using YOLO v3 Tiny with Voice Feedback for Visually Impaired
+Development of a Safe Walking Assistance System for Visually Impaired Persons Using MY VISION
+Design and Implementation of an Embedded Real-Time System for Guiding Visually Impaired Individuals
+A Blind navigation method in indoors/outdoors areas
+Implementation of a Blind navigation method in outdoors/indoors areas
+Assisting Blind People Using Object Detection with Vocal Feedback
+Deblur-YOLO: Real-Time Object Detection with Efficient Blind Motion Deblurring
+Web-based object detection and sound feedback system for visually impaired people
+A Frame-work assisting the Visually Impaired People: Common Object Detection and Pose Estimation in Surrounding Environment
+Seeing Without Sight — An Automatic Cognition System Dedicated to Blind and Visually Impaired People
+ORCap: Object Recognition Cap (A navigation system for the blind)
+Design and Implementation of Real-time Object Detection for Blind using Convolutional Neural Network
+A Mobile Application For The Visually Handicapped
+Guiding Visually Impaired People to Find an Object by Using Image to Speech over the Smart Phone Cameras
+SceneRecog: A Deep Learning Scene Recognition Model for Assisting Blind and Visually Impaired Navigate using Smartphones
+Computer vision assisted, real time blind spot detection based collision warning system for two wheelers
+Smartphone Based Tactile Feedback System Providing Navigation and Obstacle Avoidance to the Blind and Visually Impaired
+My Mobile Knows That I am Driving! In-Vehicle (Relative) Blind Localization of a Smartphone
+Intelligent Assistance System for Visually Impaired/Blind People (ISVB)
+Object Detection and Video Analyser for the Visually Impaired
+Smart Vision based Assistant for Visually Impaired
+Reforming Warning and Obstacle Detection Assisting Visually Impaired People on mHealth
+Real-Time Object Detection for Visually Challenged People
+Single Shot Detector Based Object Detection For Visually Impaired People
+Blind Lane Detection Algorithm Based on Improved YOLOv5s
+Obstacle Avoidance for blind people using Yolo algorithm, Darknet and GTTS
+Blind Assistance System using Tensor Flow
+Smart Navigation Detection using Deep-learning for Visually Impaired Person
+Guardian - Smart Assistant Tool for Visually Impaired People
+Road Pothole Detection Algorithm and Guide Belt Designed for Visually Impaired
+An Real Time Object Detection Method for Visually Impaired Using Machine Learning
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+interessante ler:
+Ultra-Modern Walking Stick Designed For The Blind
+A Soundbite-Based Framework for Text and Object Detection Using OCR and YOLO Technique to Assist Blind and Deaf
+Visually Impaired Indoor Navigation using YOLO Based Object Recognition, Monocular Depth Estimation and Binaural Sounds
+Third Eye – A Smart Wearable Glass With Deep Learning Technology Powered With Artificial Intelligence for Visually Impaired People
+
+
+
+LidSonic V2.0: A LiDAR and Deep-Learning-Based Green Assistive Edge Device to Enhance Mobility for the Visually Impaired
+LIDAR BASED GUIDING DEVICE FOR VISUALLY IMPAIRED PEOPLE
+AI Powered Glasses Sync-in With Android Application For Visually Impaired Person
+Blind Navigation Support System using Raspberry Pi & YOLO
+CREATION: Computational ConstRained Travel Aid for Object Detection in Outdoor eNvironment
+Teleassistant services using the 5G network on a remote guide for the blind persons
+Method of Non-blind-zone Foreign Object Detection in Wireless Power Transfer System
+Bipolar Checkerboard Metal Object Detection Without Blind Zone Caused by Excitation Magnetic Field for Stationary EV Wireless Charging System
+A Novel Metal Object Detection System using Asymmetric Triangular Gradiometers for High-power Inductive Power Transfer Applications
+Robot Eye: Automatic Object Detection And Recognition Using Deep Attention Network to Assist Blind People
+Clear Vision - Obstacle detection using Bat Algorithm Optimization Technique
+Blind-Zone-Free Metal Object Detection for Wireless EV Chargers Employing DD Coils by Passive Electromagnetic Sensing
+Network-Aware 5G Edge Computing for Object Detection: Augmenting Wearables to “See” More, Farther and Faster
+LiEBiD - A LIDAR based Early Blind Spot Detection and Warning System for Traditional Steering Mechanism
+Ultrasonic Smart Spectacles for Visually Impaired and Blind People
+Design and Implementation of The Blind Navigation Aids Using Ultrasonic Sensor
+A Deep Learning Framework for Blind Time-Frequency Localization in Wideband Systems
+Obstacle detection and navigation support Using SMART Shoe for Visually Impaired Person
+Implementation of object detecting system for visually impaired using Arduino
+
+
+2018
+Android Smartphone Based Visual Object Recognition for Visually Impaired Using Deep Learning
