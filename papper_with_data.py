8f5c0952608def700784927c9eb64b37fc73b5ef
Arthur Sendas F Almeida <73764748+guerraepaz@users.noreply.github.com>
2023-09-05 02:29:17 -0300

    Update papper_with_data.py

--- papper_with_data.py
+++ papper_with_data.py
@@ -1,25 +1,226 @@
 Artigos:
 A Smartphone-Based Mobility Assistant Using Depth Imaging for Visually Impaired and Blind - 2022 - with dataset COCO
+ Evaluation of Object Detection Module
+Objects	Average Confidence Scores	
+AC Remote	72.31%	
+Bottle	96.59%	
+Cup	92.01%	
+Scissors	65.23%	
+Mouse	86.72%	
+
 Real Time Object Detection with Audio Feedback using Yolo vs. Yolo_v3 - 2021 - with dataset COCO
-Real-Time Object Detection And Identification For Visually Challenged People Using Mobile Platform - 2021 - with Dataset Coco end Dota
+TABLE II. Performance of Algorithms with Single Object
+YOLO YOLO_V3
+Precision % 86.44 96.50
+Recall % 84.90 94.98
+Inference
+Time 1.80 Sec 0.345 Sec
+TABLE III Performance of Algorithms with Multiple Object
+YOLO YOLO_V3
+Precision % 83.22 93.44
+Recall % 82.12 91.45
+Inference Time 6.25 Sec 2.59 Sec
+TABLE IV. Performance of Algorithms with Distant Objects
+YOLO YOLO_V3
+Precision % 71.44 89.45
+Recall % 70.78 87.98
+Inference Time 5.76 sec 3.37 Sec
+TABLE V. Performance of Algorithms with Video Objects
+YOLO YOLO_V3
+Precision % 78.99 92.89
+Recall % 75.78 90.45
+Time 7.48 Sec 5.14 Sec
+
 Virtual AI Assistant for Person with Partial Vision Impairment - 2021 - with dataset COCO
+Object Recognition
+The object recognition module has been implemented
+successfully
+ Accuracy of 90 %
+ Average run time of 1.3 seconds
+Face Recognition
+Face Recognition module has been implemented
+successfully
+ Accuracy of 85%
+ Average run time of 1.2 seconds
+Text Recognition
+Text Recognition module has been implemented
+successfully
+ Accuracy of 90 %
+ Average run time of 1.4 seconds
+
 REAL TIME OBJECT DETECTION AND RECOGNITION SYSTEM TO ASSIST THE VISUALLY IMPAIRED - 2021 - with dataset COCO
+Sample Input Accuracy Score
+Person 0.92 
+Bottle 0.90 
+Chair 0.87 
+Clock 0.91 
+Cell Phone 0.92 
+
+
+
+Electronic Orientation Aid for Visually Impaired using Graphics Processing Unit (GPU) - 2020 - with dataset COCO
+Person 90%
+Person 78%
+Laptop 45%
+Person 92%
+Bottle 79%
+TV monitor 88%
+Person 95%
+Book 87%
+Person 80%
+  
+  
+Deblur-YOLO: Real-Time Object Detection with Efficient Blind Motion Deblurring - 2021 - with dataset COCO
+
+                 mAP   aero bike bird boat bottle bus   car   cat   chair  cow   table  dog    horse  mbike  person   plant   sheep   sofa   train    tv
+Clean Image      58.5  73.7 46.9 44.4 40.6 42.3   75.2  58.5  73.0  39.8   52.3  41.4   73.2   77.5   61.7   69.1     42.4    59.9    52.1   75.4     69.9
+Blur Image       29.7  43.3 16.9 15.3 14.8 10.4   51.8  34.5  40.4  13.5   12.6  26.9   31.7   30.9   28.2   42.8     19.0    23.7    33.7   57.6     45.4
+DeepDeblur       51.7  64.9 36.9 35.2 35.3 32.2   73.1  53.7  70.3  33.9   40.8  40.1   59.6   68.1   51.9   65.3     35.2    46.8    48.5   72.8     68.4
+DynamicDeblur    56.0  70.6 43.2 41.4 41.4 36.8   75.3  57.1  72.6  36.6   45.8  40.0   68.2   72.7   56.3   67.0     39.8    58.4    49.9   75.3     71.7
+SRN              52.3  70.1 38.2 35.8 35.8 31.8   71.9  53.4  69.2  33.1   39.4  39.8   63.4   66.6   53.5   64.1     35.1    51.7    48.0   75.3     69.0
+DeblurGANv2(I-R) 42.0  55.0 28.6 26.6 30.2 24.9   61.4  44.9  53.5  27.5   35.4  32.4   47.4   53.7   39.6   51.8     24.8    41.2    39.2   65.2     55.9
+DeblurGANv2(M)   40.8  52.2 27.4 25.0 28.9 24.3   61.0  44.3  53.7  25.9   31.7  30.5   45.2   49.4   39.2   50.8     25.0    38.6    40.6   66.0     56.8
+Deblur-Yolo      47.5  55.5 33.8 30.0 37.7 29.7   67.7  51.1  62.6  31.2   39.5  41.2   51.4   54.7   44.9   56.1     33.6    53.9    50.2   72.8     52.2
+
+
+Design and Implementation of Real-time Object Detection for Blind using Convolutional Neural Network - 2022 - with dataset COCO
+T ABLE 3. T HE RESULT OF O BJECT D ETECTION T EST
+Object Target
+True Positive
+False Positive
+Percentage Correct
+Human 29 1 96,6%
+Chair 19 1 95%
+Television 18 2 90%
+Laptop 18 2 90%
+Handphone 19 1 95%
+Car 19 1 95%
+Book 19 1 95%
+A Mobile Application For The Visually Handicapped - 2021 - with dataset COCO
+  A clock is detected (96%)
+  A scissor is detected (%87).
+  A laptop is detected (%94)
+  A cat is detected (%90)
+  A refrigerator is detected (%81)
+  A remote is detected (%81)
+  A laptop is detected (%75).
+  A scissor is detected (%79).
+A vehicle is detected (%70)
+A book is detected (%70)
+
+
+Intelligent Assistance System for Visually Impaired/Blind People (ISVB) - 2022 - with dataset COCO
+TABLE II: Evaluation of different objects detected by the camera through success and fail rate.
+Objects No. Tests Accuracy
+Chair 245 97.9%
+Person 200 96.5%
+Water Bottle 120 97.7%
+Traffic lights 60 90.9%
+Utensils 260 97.2%
+Food 300 96.5%
+Mobile phones 230 98.6%
+
+Chair 0.81%
+Person 0.63%
+Fork 0.83%
+Cell phone 0.76%
+Bottle 0.83%
+Broccoli 0.75%
+
+Smart Vision based Assistant for Visually Impaired - 2022 -  with dataset COCO
+
+Test Cases Object Predicted
+Object
+Failure
+Cases Accuracy
+1 Person Person None 99%
+2 Person
+Cellphone
+Person
+Cellphone None 97%
+97%
+3
+Bottle
+Scissors
+Toothbrush
+Comb
+Bottle
+Scissors
+Toothbrush
+Comb
+79%
+90%
+83%
+4 Teddybear Teddybear None 91%
+5 Cellphone
+Fork
+Cellphone
+Fork None 93%
+93%
+6
+Teddybear
+Remote
+Cellphone
+Teddybear
+Remote
+Cellphone
+None
+70%
+80%
+85%
+7 Knife
+Mouse
+Knife
+Mouse None 85%
+87%
+8 Laptop
+Cup
+Laptop
+Cup None Above
+75%
+9
+Knife
+Spoon
+Banana
+Knife
+Spoon Banana Above
+70%
+  
+
+Real-Time Object Detection for Visually Challenged People - 2020 - with dataset COCO
+Chair 79%
+Remote Control 62%
+Cell Phone 77%
+Person 77%
+
+
+Single Shot Detector Based Object Detection For Visually Impaired People - 2022 - with dataset COCO
+
+Person 90%
+Bed 80%
+Bottle 82%
+Cup 96%
+Cell Phone 98%
+Teddy Bear 97%
+
+Obstacle Avoidance for blind people using Yolo algorithm, Darknet and GTTS - 2023 - with dataset COCO
+
+Cell Phone 75.52%
+Laptop 61.06%
+Truck 0.4%
+Chair 0.5%
+Car 0.4%
+Person 0.8%
+  
+
+An Real Time Object Detection Method for Visually Impaired Using Machine Learning - 2023 - with dataset COCO
+Real-Time Object Detection And Identification For Visually Challenged People Using Mobile Platform - 2021 - with Dataset Coco end Dota
 Guiding Aid for Visually Impaired - 2022 - with dataset COCO
 Assistive Object Recognition System for Visually Impaired - 2020 - with dataset COCO
-Electronic Orientation Aid for Visually Impaired using Graphics Processing Unit (GPU) - 2020 - with dataset COCO
-Deblur-YOLO: Real-Time Object Detection with Efficient Blind Motion Deblurring - 2021 - with dataset COCO
 ORCap: Object Recognition Cap (A navigation system for the blind) - 2020 - with dataset COCO
-Design and Implementation of Real-time Object Detection for Blind using Convolutional Neural Network - 2022 - with dataset COCO
-A Mobile Application For The Visually Handicapped - 2021 - with dataset COCO
 Guiding Visually Impaired People to Find an Object by Using Image to Speech over the Smart Phone Cameras - 2021 - with dataset COCO
-Intelligent Assistance System for Visually Impaired/Blind People (ISVB) - 2022 - with dataset COCO
-Object Detection and Video Analyser for the Visually Impaired - 2023 - with dataset COCO
-Smart Vision based Assistant for Visually Impaired - 2022 -  with dataset COCO
 Reforming Warning and Obstacle Detection Assisting Visually Impaired People on mHealth - 2019 - with dataset COCO
-Real-Time Object Detection for Visually Challenged People - 2020 - with dataset COCO
-Single Shot Detector Based Object Detection For Visually Impaired People - 2022 - with dataset COCO
-Obstacle Avoidance for blind people using Yolo algorithm, Darknet and GTTS - 2023 - with dataset COCO
-An Real Time Object Detection Method for Visually Impaired Using Machine Learning - 2023 - with dataset COCO
+
 Android Based Object Recognition for visually impaired - 2020 - without dataset
 Efficient Multi-Object Detection and Smart Navigation Using Artificial Intelligence for Visually Impaired People - 2020 ???
 Object Detection and Narrator for Visually Impaired People - 2019 - with Dataset Imagenet
@@ -48,7 +249,7 @@ Smart Navigation Detection using Deep-learning for Visually Impaired Person - 20
 Guardian - Smart Assistant Tool for Visually Impaired People - 2022 - without dataset
 Road Pothole Detection Algorithm and Guide Belt Designed for Visually Impaired - 2023 - with dataset Kaggle
 Android Smartphone Based Visual Object Recognition for Visually Impaired Using Deep Learning - 2018 - without dataset
-
+Object Detection and Video Analyser for the Visually Impaired - 2023 - with dataset Flickr-30k
 
 
 
